3ec3befddbc3b7f75a3492cae2e2f882a51fde5e
Zee Zali <zee.zali22@gmail.com>
2015-05-06 02:53:34 +1000

    Wrap everything in UMD wrapper. Make error and success callbacks optional

--- index.js
+++ index.js
@@ -1,71 +1,94 @@
 
-
-'use strict';
-
-
-// options object
-// { url: '', sucess: func, error: func }
-
-var SVGStoreLoader = function(options) {
-
-    this.url = options.url;
-    this.successCallback = options.success;
-    this.errorCallback = options.error;
-
-    this.loadFile();
-    
-};
+// umd wrapper
+// https://github.com/umdjs/umd
+// see "returnExports.js" example
 
 
-SVGStoreLoader.prototype = {
+(function (root, factory) {
 
-    constructor: SVGStoreLoader,
+    if (typeof define === 'function' && define.amd) {
+        // amd. register as an anonymous module.
+        define([], factory);
 
-    configureSVG: function() {
-        var $body = document.body;
+    } else if (typeof exports === 'object') {
+        // commonjs-like environments that support module.exports (like node)
+        module.exports = factory();
 
-        this.$returnElem = document.createElement('div');
-        this.$returnElem.className = 'svg-store';
-        this.$returnElem.setAttribute('aria-hidden', true);
-        this.$returnElem.innerHTML = this.responseText;
-        this.$returnElem.style.display = 'none';
+    } else {
+        // browser globals (root is window)
+        // window.SVGStoreLoader
+        root.SVGStoreLoader = factory();
 
-        // prepend to the body
-        $body.insertBefore(this.$returnElem , $body.firstChild);
-    },
-
-    onError: function() {
-        this.errorCallback();
-    },
-
-    onSuccess: function() {
-        this.configureSVG();
-        this.successCallback();
-    },
-
-    loadFile: function() {
-        var self = this;
-        var xmlhttp = new XMLHttpRequest();
-
-        xmlhttp.onreadystatechange = function() {
-            if (xmlhttp.readyState == 4 ) {
-
-               if (xmlhttp.status == 200) {
-                    self.responseText = xmlhttp.responseText;
-                    self.onSuccess();
-               } else {
-                    self.onError();
-               }
-
-            }
-        };
-
-        xmlhttp.open("GET", this.url, true);
-        xmlhttp.send();
     }
 
-};
+}(this, function() {
+    
+    'use strict';
+
+    // options object
+    // { url: '', sucess: func, error: func }
+
+    var SVGStoreLoader = function(options) {
+
+        this.url = options.url;
+        this.successCallback = options.success;
+        this.errorCallback = options.error;
+
+        this.loadFile();
+
+    };
 
 
-module.exports = SVGStoreLoader;
+    SVGStoreLoader.prototype = {
+
+        constructor: SVGStoreLoader,
+
+        configureSVG: function() {
+            var $body = document.body;
+
+            this.$returnElem = document.createElement('div');
+            this.$returnElem.className = 'svg-store';
+            this.$returnElem.setAttribute('aria-hidden', true);
+            this.$returnElem.innerHTML = this.responseText;
+            this.$returnElem.style.display = 'none';
+
+            // prepend to the body
+            $body.insertBefore(this.$returnElem , $body.firstChild);
+        },
+
+        onError: function() {
+            if (this.errorCallback) this.errorCallback();
+        },
+
+        onSuccess: function() {
+            this.configureSVG();
+            if (this.successCallback) this.successCallback();
+        },
+
+        loadFile: function() {
+            var self = this;
+            var xmlhttp = new XMLHttpRequest();
+
+            xmlhttp.onreadystatechange = function() {
+                if (xmlhttp.readyState == 4 ) {
+
+                   if (xmlhttp.status == 200) {
+                        self.responseText = xmlhttp.responseText;
+                        self.onSuccess();
+                   } else {
+                        self.onError();
+                   }
+
+                }
+            };
+
+            xmlhttp.open("GET", this.url, true);
+            xmlhttp.send();
+        }
+
+    };
+
+    return SVGStoreLoader;
+
+}));
 
